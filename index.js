18e65a3aa5e85e2ebc17ca26ca0689932c1b6629
Yeji-Yoon <angelayoon0102@gmail.com>
2025-01-28 09:35:34 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(whatTodo = "roller-skate") {
+    return `This Saturday, I want to ${whatTodo}!`
+  }
+
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+
+
+
+function wrapAdjective(putSomething= "special") {
+    const innerFunction = function (whatAreyou) {
+      return `You are ${putSomething}${whatAreyou}${putSomething}!`;
+    };
+    return innerFunction;
+  }
+
+wrapAdjective("%")("a dedicated programmer"); 
+    
